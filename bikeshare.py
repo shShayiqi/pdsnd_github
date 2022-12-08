af9242957cd497cfd479f4b5027240192f3cb4ca
shahad <shahad143388@hotmail.com>
2022-12-08 23:05:41 +0300

    add command

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ import calendar
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+# 1st update 
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
