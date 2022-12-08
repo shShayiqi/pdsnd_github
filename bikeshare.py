db7d3579ccbbdd66a2673c8dd9859cc7788b326b
shahad <shahad143388@hotmail.com>
2022-12-08 23:11:55 +0300

    update bikshare file

--- bikeshare.py
+++ bikeshare.py
@@ -7,6 +7,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+# update file for refactoring branch 
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
